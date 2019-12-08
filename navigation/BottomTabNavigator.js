39328185ab90d02cdc9e7798e6018cddab49cb57
Arthur <arthurbelyankove@gmail.com>
2019-12-08 19:36:27 +0600

    navigation done

---
+++ navigation/BottomTabNavigator.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import ScreenHome from '../screens/ScreenHome';
+import ScreenMentors from '../screens/ScreenMentors';
+import ScreenReports from '../screens/ScreenReports';
+import ScreenProfile from '../screens/ScreenProfile';
+
+const BottomTabNavigator = createBottomTabNavigator({
+  Главная: ScreenHome,
+  Наставники: ScreenMentors,
+  Отчёты: ScreenReports,
+  Профиль: ScreenProfile
+}, {
+  tabBarOptions: {
+    showLabel: true
+  }
+});
+
+export default BottomTabNavigator;
---
+++ screens/ScreenReports.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+import { Ionicons } from '@expo/vector-icons';
+
+const TabIcon = (props) => (
+    <Ionicons
+        name={'md-list'}
+        size={30}
+        color={props.focused ? 'blue' : 'darkgrey'}
+    />
+);
+
+export default class ScreenProfile extends React.Component {
+
+    static navigationOptions = {
+        tabBarIcon: TabIcon
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Screen Reports</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
