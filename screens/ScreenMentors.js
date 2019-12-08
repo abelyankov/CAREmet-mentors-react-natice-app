c9f8601c9eeae61aec795c762a29db24f4717eeb
Arthur <arthurbelyankove@gmail.com>
2019-12-09 05:01:35 +0600

    Report list minor update

--- screens/ScreenMentors.js
+++ screens/ScreenMentors.js
@@ -1,141 +1,18 @@
 import React from 'react';
 import { View, StyleSheet, FlatList, Text, TouchableOpacity } from 'react-native';
+import {MentorList} from '../lists/MentorList'
 
-const MentorList = [
-    {
-        key: '1',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '2',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 26,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '3',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '4',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '5',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '6',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '7',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '8',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '9',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '10',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-    {
-        key: '11',
-        first_name: 'Сергей',
-        last_name: 'Иванов',
-        age: 25,
-        phone_number: '+7(777)111-11-11',
-        work: 'АО "КБТУ"',
-        workPosition: 'Программист',
-        hobby: 'Любит играть в гиры',
-        about: 'Нормальный человек...'
-    },
-];
 
 function Item({first_name, last_name, age, about}) {
     return (
         <View style={styles.item}>
-            <Text style={styles.full_name}>{first_name} {last_name} <Text style={styles.age}>({age} лет)</Text> </Text>
+            <Text style={styles.full_name}>{first_name} {last_name} <Text style={styles.age}>({age}лет)</Text> </Text>
             <Text style={styles.about}>О себе: {about} </Text>
         </View>
     );
 }
 
 export default class ScreenMentors extends React.Component {
-
     render() {
         return (
             <View style={styles.container}>
@@ -163,7 +40,7 @@ export default class ScreenMentors extends React.Component {
                                                     about = {item.about}/>
                         </TouchableOpacity>
                     }
-                    keyExtractor={item => item.key}
+                    keyExtractor={item =>item.key}
                 />
             </View>
         );
--- screens/ScreenReports.js
+++ screens/ScreenReports.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import {View, StyleSheet, Text, FlatList} from 'react-native';
+import {ReportList} from "../lists/ReportList";
+
+
+function Item({title, description, status,created_at, author, event}) {
+    let label;
+    if (status === "accepted") {
+        label =  <View style={styles.green_label}><Text style={styles.status}>Принят</Text></View>
+    } else {
+        label = <View style={styles.red_label}><Text style={styles.status}>Отклонён</Text></View>
+    }
+    return (
+        <View style={styles.item}>
+            {label}
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+            </View>
+            <View style={styles.body}>
+                <Text style={styles.description}> {description} </Text>
+            </View>
+            <Text style={styles.created_at}> {created_at}</Text>
+        </View>
+    );
+}
 
 export default class ScreenProfile extends React.Component {
-
     render() {
         return (
             <View style={styles.container}>
-                <Text>Screen HELLO</Text>
+                <FlatList
+                    data={ReportList}
+                    renderItem={({ item }) =>
+                            <Item title={item.title}
+                                  description={item.description}
+                                  status = {item.status}
+                                  author = {item.author}
+                                  event = {item.event}
+                                  key = {item.key}
+                                  created_at={item.created_at}/>
+                    }
+                    keyExtractor={item =>item.key}
+                />
             </View>
         );
     }
@@ -14,8 +48,35 @@ export default class ScreenProfile extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        flex: 1
     },
+    item: {
+        marginVertical: 5,
+        marginHorizontal: 10,
+        borderWidth: 0.5,
+        borderRadius: 5,
+        borderColor: '#ccc',
+        flex: 1
+    },
+    green_label: {
+        borderTopLeftRadius: 5,
+        borderTopRightRadius: 5,
+        fontSize: 17,
+        backgroundColor: 'green',
+        padding: -10,
+        
+    },
+
+    title: {
+        fontSize: 18
+    },
+    header: {
+        flexDirection: 'row'
+    },
+    created_at: {
+        fontSize: 14,
+        opacity: 0.3,
+        textAlign: 'right',
+        paddingRight: 10
+    }
 });
